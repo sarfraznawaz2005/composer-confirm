5dd9b8b40618a6148fa650444c616b4e972685ad
Sarfraz <sarfraz@eteamid.com>
2018-01-29 15:56:01 +0500

    updated

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+git status
+git add .
+read commitMessage
+git commit -am "$commitMessage"
+git push
+# update tags
+git push --delete origin 1.0.0
+git tag --delete 1.0.0
+git tag 1.0.0
+git push --tags
+echo Press Enter...
+read
